f92720b8cea94fe1aafb309f51d840aa0f4be1d5
otherworldly94b <and.charalambous@gmail.com>
2023-06-02 20:58:52 +0000

    Add main game function and event listener for the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,31 @@ const messages = documemt.getElementsById('messages');
 const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 /**
- * Add event listener to the buttons and test if they work with alerts
+ * Add event listener to the buttons
  */
 // code based on the Love Maths project
 for (let button of buttons) {
     button.addEventListener("click", function() {
-        if (this.getAttribute("data-type") === "rock") {
-            alert(`You clicked rock`);
-            // checkAnswer();
-        } else {
-            let gameType = this.getAttribute("data-type");
-            runGame(gameType);
-        }
+        let playerOption = this.getAttribute('data-type');
+        playGame(playerOption);
     });
+}
+
+/**
+ * The main game function. Accepts one parameter, which 
+ * is the data-type value of the selected button
+ */
+function playGame(playerOption) {
+    
+    playerImage.src = `assets/images/${choices[playerOption]}.png`;
+    playerImage.alt = choices[playerOption];
+    
+    let computerOption = Math.floor(Math.random() * 5);
+
+    computerImage.src = `assets/images/${choices[computerOption]}.png`;
+    computerImage.alt = choices[computerOption];
+
+    let result = checkWinner(choices[computerOption], choices[playerOption]);
+
+    updateScores(result);
+}
